4bc8f8ce77a5f432b2d3b7b10588abf1ed16e4a0
Théo MARCILLA <theo.marcilla@viacesi.fr>
2024-06-05 16:28:58 +0200

    Added JWT + Authentification and bearer token

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,26 +1,52 @@
-
 const express = require('express')
 const bodyParser = require('body-parser');
 const app = express()
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+dotenv.config();
+process.env.TOKEN_SECRET;
+
+function generateAccessJWT(username) {
+  return jwt.sign({username}, process.env.TOKEN_SECRET, {expiresIn: '1800s'})
+}
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
 
 app.use(bodyParser.json());
-
-/* GET home page. */
-app.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
 app.post('/login', (req, res) => { 
   const { username, password } = req.body;
 
   console.log(username, password)
   if (username && password) {
+    const token = generateAccessJWT(username);
+    res.json({token});
     res.status(200).send(`Username: ${username}, Password: ${password}`);
 } else {
     res.status(400).send('Username and Password are required');
 }
 });
 
+app.get('/protected', authenticateToken, (req, res) => {
+  res.send(`Hello, ${req.user.username}. You are authenticated!`);
+});
+
+
+app.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
 const port = 3001;
 app.listen(port, () => {
