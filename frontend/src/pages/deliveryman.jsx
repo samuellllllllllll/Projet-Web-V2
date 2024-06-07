d9c3aabcbf180590f2037ed056d6054c2f65108c
Samuel Jarjanette <jarjanette.samuel@orange.fr>
2024-06-07 16:10:05 +0200

    debut livreur

--- frontend/src/pages/deliveryman.jsx
+++ frontend/src/pages/deliveryman.jsx
@@ -1,9 +1,21 @@
+import React, { useState } from 'react';
+import '../styles/deliveryman.css';
+import logo from '../assets/logo.png';
+import menuIcon from '../assets/menu.png';
+
 const deliveryman = () => {
 
   return (
-    <>
-      <h1>Deliveryman</h1>
-    </>
+    <div className="menu-page">
+      <header className="menu-header">
+        <img src={logo} alt="logo CESI'Eats" className="logo" />
+        <img src={menuIcon} alt="Menu Icon" className="menuIcon" />
+      </header>
+      <h1>Choisissez une commande à livrer </h1>
+      <th>Détail</th>
+      <th>Heure récupération</th>
+      <th>ID</th>
+    </div>
   )
 }
 
