8b905acfe403ef20ac57405278352f2780f35d3b
Victor <vicbil33bdx@gmail.com>
2024-06-19 16:53:21 +0200

    Feature account

--- frontend/src/pages/consumer/account.jsx
+++ frontend/src/pages/consumer/account.jsx
@@ -1,25 +1,61 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "../../styles/consumer/account.css";
 import Footer from '../../components/Footer.jsx';
 import Header from '../../components/Header.jsx';
+import axios from 'axios';
 
 const Account = () => {
 
     const [isEditing, setIsEditing] = useState(false);
     const [user, setUser] = useState({
-        lastName: "BILLAT",
-        firstName: "Victor",
-        phone: "06 12 34 56 78",
-        email: "victor@cesieats.fr",
-        address: "1 rue de la paix 75000 Paris"
+        phone: "",
+        email: "",
+        city: "",
+        postal_code: "",
+        street: "",
+        number: "",
+        country: "",
+        password: "",
     });
 
     const handleEditClick = () => {
         setIsEditing(true);
     };
 
-    const handleSaveClick = () => {
+    const handleSaveClick = async () => {
         setIsEditing(false);
+
+        // Update the user informations
+        try{
+            const response = await axios.put('http://localhost:4549/account/consumer/update', {
+                params: {
+                    phone: user.phone,
+                    email: user.email,
+                    address_city: user.city,
+                    address_postal_code: user.postal_code,
+                    address_street: user.street,
+                    address_number: user.number,
+                    adresse_country: user.country,
+                    id: 1 // TO BE CHANGED
+                }
+            });
+        } catch (error) {
+            console.error('Error updating data:', error);
+        }
+    };
+
+    const handleDeleteClick = async () => {
+        // Delete the user account
+        try{
+            const response = await axios.put('http://localhost:4549/account/consumer/delete', {
+                params: {
+                    id: 1 // TO BE CHANGED
+                }
+            });
+        }
+        catch (error) {
+            console.error('Error deleting data:', error);
+        }
     };
 
     const handleInputChange = (e) => {
@@ -30,6 +66,23 @@ const Account = () => {
         });
     };
 
+    const loadInfoUser = async () => {
+        try {
+            const response = await axios.get('http://localhost:4549/account/consumer', {
+                params: { id: 1 }, // TO BE CHANGED
+            });
+            // Change the data in user
+            setUser(response.data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    // Load user informations when the page is loaded
+    useEffect(() => {
+        loadInfoUser();
+    }, []);
+
     return (
         <div className="account">
             <Header />
@@ -39,19 +92,48 @@ const Account = () => {
                     {isEditing && <button className="edit-button" onClick={handleSaveClick}>Sauvegarder</button>}
                 </div>
                 <div className="account-details">
-                    <div className="account-details-left">
-                        <p>Nom</p>
-                        <p>Prénom</p>
-                        <p>Téléphone</p>
-                        <p>Email</p>
-                        <p>Adresse</p>
+                    {isEditing && 
+                    <div className="delete-button-div">
+                        <button className="delete-button" onClick={handleDeleteClick}>Supprimer mon compte</button>
                     </div>
-                    <div className="account-details-right">
-                        {isEditing ? <input type="text" name="lastName" value={user.lastName} onChange={handleInputChange} /> : <p>{user.lastName}</p>}
-                        {isEditing ? <input type="text" name="firstName" value={user.firstName} onChange={handleInputChange} /> : <p>{user.firstName}</p>}
-                        {isEditing ? <input type="text" name="phone" value={user.phone} onChange={handleInputChange} /> : <p>{user.phone}</p>}
-                        {isEditing ? <input type="text" name="email" value={user.email} onChange={handleInputChange} /> : <p>{user.email}</p>}
-                        {isEditing ? <input type="text" name="address" value={user.address} onChange={handleInputChange} /> : <p>{user.address}</p>}
+                    }
+                    <div className="account-details-bottom">
+                        <div className="account-details-left">
+                            <p>Téléphone</p>
+                            <p>Email</p>
+                            <p>Mot de passe</p>
+                            <p>Pays adresse</p>
+                            <p>Ville adresse</p>
+                            <p>Code postal adresse</p>
+                            <p>Rue adresse</p>
+                            <p>Numéro adresse</p>
+                        </div>
+                        <div className="account-details-right">
+                            {isEditing ? (
+                                <>
+                                    <input name="phone" type="text" value={user.phone} onChange={handleInputChange} />
+                                    <input name="email" type="text" value={user.email} onChange={handleInputChange} />
+                                    <input name="password" type="text" value={user.password} onChange={handleInputChange} />
+                                    <input name="country" type="text" value={user.country} onChange={handleInputChange} />
+                                    <input name="city" type="text" value={user.city} onChange={handleInputChange} />
+                                    <input name="postal_code" type="text" value={user.postal_code} onChange={handleInputChange} />
+                                    <input name="street" type="text" value={user.street} onChange={handleInputChange} />
+                                    <input name="number" type="text" value={user.number} onChange={handleInputChange} />
+                                    
+                                </>
+                            ) : (
+                                <>
+                                    <input name="phone" type="text" className="disabled-input" value={user.phone} disabled />
+                                    <input name="email" type="text" className="disabled-input" value={user.email} disabled />
+                                    <input name="password" type="text" className="disabled-input" value={user.password} disabled />
+                                    <input name="country" type="text" className="disabled-input" value={user.country} disabled />
+                                    <input name="city" type="text" className="disabled-input" value={user.city} disabled />
+                                    <input name="postal_code" type="text" className="disabled-input" value={user.postal_code} disabled />
+                                    <input name="street" type="text" className="disabled-input" value={user.street} disabled />
+                                    <input name="number" type="text" className="disabled-input" value={user.number} disabled />
+                                </>
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
